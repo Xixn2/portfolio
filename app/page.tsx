cf7b475a17a74eee101b0bbd8ce2ef33b694bac1
Jiwan SEO <ssbkh0802@naver.com>
2024-11-28 16:13:22 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect(
-    "https://jiwanios.notion.site/dd3a8ff193b0490cad81acd61cb8aa97?pvs=4"
+    "https://seojiwan.notion.site/portfolio"
   );
 }
